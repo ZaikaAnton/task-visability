c15f210d8dc66ab814e09dad787ce306b67447ef
Anton Zaika <lycan44444@gmail.com>
2024-06-28 23:35:06 +0200

    fix1.2

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,36 +1,36 @@
-// import React from "react";
-// import { useEffect } from "react";
-// import { useDocumentVisibility } from "../../node_modules/anton-usedocumentvisibility/useDocumentVisibility1/useDocumentVisibility";
-// import Example from "./Example";
-// import SecondExample from "./SecondExample";
+import React from "react";
+import { useEffect } from "react";
+import { useDocumentVisibility } from "../../node_modules/anton-usedocumentvisibility/useDocumentVisibility1/useDocumentVisibility";
+import Example from "./Example";
+import SecondExample from "./SecondExample";
 
-// const App: React.FC = () => {
-//   const { count, visible, onVisibilityChange } = useDocumentVisibility();
+const App: React.FC = () => {
+  const { count, visible, onVisibilityChange } = useDocumentVisibility();
 
-//   useEffect(() => {
-//     onVisibilityChange((isVisible) => {
-//       console.log("first handler", isVisible);
-//     });
+  useEffect(() => {
+    onVisibilityChange((isVisible) => {
+      console.log("first handler", isVisible);
+    });
 
-//     const unsubscribeSecondHandler = onVisibilityChange((isVisible) => {
-//       console.log("second handler", isVisible);
-//     });
+    const unsubscribeSecondHandler = onVisibilityChange((isVisible) => {
+      console.log("second handler", isVisible);
+    });
 
-//     setTimeout(() => unsubscribeSecondHandler(), 5000); // отписываемся от 'second handler' через 5 секунд
-//   }, [onVisibilityChange]);
+    setTimeout(() => unsubscribeSecondHandler(), 5000); // отписываемся от 'second handler' через 5 секунд
+  }, [onVisibilityChange]);
 
-//   return (
-//     <>
-//       <div>
-//         <span>
-//           Вы покинули страницу: {count} раз <br />
-//           Вкладка активна? {visible ? "да" : "нет"}
-//         </span>
-//       </div>
-//       <Example />
-//       <SecondExample />
-//     </>
-//   );
-// };
+  return (
+    <>
+      <div>
+        <span>
+          Вы покинули страницу: {count} раз <br />
+          Вкладка активна? {visible ? "да" : "нет"}
+        </span>
+      </div>
+      <Example />
+      <SecondExample />
+    </>
+  );
+};
 
-// export default App;
+export default App;
--- src/hook/useMediaQuery.ts
+++ src/hook/useMediaQuery.ts
@@ -1,29 +1,29 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-// interface ParamQuery {
-//   query: string;
-// }
+interface ParamQuery {
+  query: string;
+}
 
-// export function useMediaQuery({ query }: ParamQuery): boolean {
-//   const [matches, setMatches] = useState(
-//     () => window.matchMedia(query).matches
-//   );
+export function useMediaQuery({ query }: ParamQuery): boolean {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
 
-//   useEffect(() => {
-//     const mediaQueryList = window.matchMedia(query);
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(query);
 
-//     const handleChange = (event: MediaQueryListEvent) => {
-//       setMatches(event.matches);
-//     };
+    const handleChange = (event: MediaQueryListEvent) => {
+      setMatches(event.matches);
+    };
 
-//     setMatches(mediaQueryList.matches);
+    setMatches(mediaQueryList.matches);
 
-//     mediaQueryList.addEventListener("change", handleChange);
+    mediaQueryList.addEventListener("change", handleChange);
 
-//     return () => {
-//       mediaQueryList.removeEventListener("change", handleChange);
-//     };
-//   }, [query]);
+    return () => {
+      mediaQueryList.removeEventListener("change", handleChange);
+    };
+  }, [query]);
 
-//   return matches;
-// }
+  return matches;
+}
